03834d448a70021c0ab606726d10b1b940654817
Hakan <97197833+Dro-Dro@users.noreply.github.com>
2024-07-16 15:57:44 -0700

    init global variables and board

--- tictactoe.py
+++ tictactoe.py
@@ -1,3 +1,17 @@
 import pygame as pg,sys
 from pygame.locals import *
 import time
+
+#init global variables
+XO = 'x'
+winner = None
+draw = False
+width = 400 #400 is the number of pixels for length
+height = 400
+white = (255, 255, 255) #nums are variables for white
+line_color = (10, 10, 10) #nums are variables for black
+
+#init the game board
+TTT = [[None]*3, [None]*3, [None]*3]
+
+

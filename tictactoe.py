2263ce4dfd8b2ecc12e6a2854a7dd2eaf937f6ba
Hakan <97197833+Dro-Dro@users.noreply.github.com>
2024-07-16 15:59:58 -0700

    init pygame window + removed height v

--- tictactoe.py
+++ tictactoe.py
@@ -7,11 +7,15 @@ XO = 'x'
 winner = None
 draw = False
 width = 400 #400 is the number of pixels for length
-height = 400
 white = (255, 255, 255) #nums are variables for white
 line_color = (10, 10, 10) #nums are variables for black
 
 #init the game board
 TTT = [[None]*3, [None]*3, [None]*3]
 
-
+#init pygame window
+pg.init()
+fps = 30
+CLOCK = pg.time.Clock()
+screen = pg.display.set_mode((width, width + 100), 0, 32)
+pg.display.set_caption("Tic Tac Toe!")

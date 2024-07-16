a48cf7d6dc94525063f29acfca0eac0b88adfd42
Hakan <97197833+Dro-Dro@users.noreply.github.com>
2024-07-16 16:12:24 -0700

    defined draw status

--- tictactoe.py
+++ tictactoe.py
@@ -42,3 +42,23 @@ def game_opening() :
     #draws horizontal lines for board
     pg.draw.line(screen, line_color, (0, width / 3), (width, width / 3), 7)
     pg.draw.line(screen, line_color, (0, width / 3 * 2), (width, width / 3 * 2), 7)
+    draw_status()
+
+def draw_status() :
+    global draw
+
+    if winner is None:
+        message = XO.upper() + "'s Turn"
+    elif draw:
+        message = 'Game Draw!'
+    else:
+        message = winner.upper() + " won!"
+
+    font = pg.font.Font(None, 30)
+    text = font.render(message, 1, white)
+
+    #copies rendered message to board
+    screen.fill ((0, 0, 0), (0, 400, 500, 100))
+    text_rect = text.get_rect(center=(width / 2, 500 - 50))
+    screen.blit(text, text_rect)
+    pg.display.update()
